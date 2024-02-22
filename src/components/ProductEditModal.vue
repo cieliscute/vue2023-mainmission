fd8c683b1c9c94049226194e6212551a89fca4d2
kaiwei <cielchang0324@gmail.com>
2024-02-23 00:03:19 +0800

    refactor: 💡 調整一下VField的位置

--- src/components/ProductEditModal.vue
+++ src/components/ProductEditModal.vue
@@ -1,20 +1,18 @@
 <template>
-  <div id="productModal" ref="productModal"
-  class="modal fade" tabindex="-1"
-  data-bs-backdrop="static" data-bs-keyboard="false"
-  aria-labelledby="productModalLabel"
-  aria-hidden="true">
+  <div id="productModal" ref="productModal" class="modal fade"
+  tabindex="-1" data-bs-backdrop="static"
+  data-bs-keyboard="false" aria-labelledby="productModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl">
-      <div class="modal-content border-0">
-        <div class="modal-header bg-dark text-white">
-          <h5 id="productModalLabel" class="modal-title">
-            <span>{{tempData.modalTitle}}產品</span>
-          </h5>
-          <button type="button" class="btn-close"
-          data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <VForm @submit="onSubmit" @keyup.enter="onSubmit" v-slot="{ errors }">
+      <VForm @submit="onSubmit" @keyup.enter="onSubmit" v-slot="{ errors }">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-dark text-white">
+            <h5 id="productModalLabel" class="modal-title">
+              <span>{{ tempData.modalTitle }}產品</span>
+            </h5>
+            <button type="button" class="btn-close"
+            data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
             <div class="row">
               <div class="col-12 col-lg-4 mb-5 mb-lg-0">
                 <div class="mb-2">
@@ -40,53 +38,42 @@
               <div class="col-12 col-lg-8">
                 <div class="mb-3">
                   <label for="title" class="form-label">標題</label>
-                  <VField id="title" type="text" class="form-control" placeholder="請輸入標題"
-                  v-model="productData.title"
-                  rules="required" name="title"
-                  :class="{ 'is-invalid': errors['title'] }"
-                  >
+                  <VField id="title" type="text" class="form-control"
+                  placeholder="請輸入標題" v-model="productData.title"
+                    rules="required" name="title" :class="{ 'is-invalid': errors['title'] }">
                   </VField>
                 </div>
                 <div class="row">
                   <div class="mb-3 col-md-6">
                     <label for="category" class="form-label">分類</label>
-                    <VField id="category" type="text"
-                    class="form-control" placeholder="請輸入分類"
-                    v-model="productData.category"
-                    rules="required" name="category"
-                    :class="{ 'is-invalid': errors['category'] }"
-                    >
+                    <VField id="category" type="text" class="form-control" placeholder="請輸入分類"
+                      v-model="productData.category" rules="required" name="category"
+                      :class="{ 'is-invalid': errors['category'] }">
                     </VField>
                   </div>
                   <div class="mb-3 col-md-6">
                     <label for="price" class="form-label">單位</label>
-                    <VField id="unit" type="text" class="form-control" placeholder="請輸入單位"
-                    v-model="productData.unit"
-                    rules="required" name="unit"
-                    :class="{ 'is-invalid': errors['unit'] }"
-                    >
+                    <VField id="unit" type="text" class="form-control"
+                    placeholder="請輸入單位" v-model="productData.unit"
+                      rules="required" name="unit" :class="{ 'is-invalid': errors['unit'] }">
                     </VField>
                   </div>
                 </div>
                 <div class="row">
                   <div class="mb-3 col-md-6">
                     <label for="origin_price" class="form-label">原價</label>
-                    <VField id="origin_price" type="number"
-                    min="0" class="form-control" placeholder="請輸入原價"
-                    v-model.number="productData.origin_price"
-                    rules="required" name="origin_price"
-                    :class="{ 'is-invalid': errors['origin_price'] }"
-                    >
+                    <VField id="origin_price" type="number" min="0"
+                    class="form-control" placeholder="請輸入原價"
+                      v-model.number="productData.origin_price" rules="required" name="origin_price"
+                      :class="{ 'is-invalid': errors['origin_price'] }">
                     </VField>
                   </div>
                   <div class="mb-3 col-md-6">
                     <label for="price" class="form-label">售價</label>
                     <VField id="price" type="number" min="0"
                     class="form-control" placeholder="請輸入售價"
-                    v-model.number="productData.price"
-                    rules="required" name="price"
-                    :class="{ 'is-invalid': errors['price'] }"
-                    >
+                      v-model.number="productData.price" rules="required" name="price"
+                      :class="{ 'is-invalid': errors['price'] }">
                     </VField>
                   </div>
                 </div>
@@ -94,36 +81,36 @@
                 <div class="mb-3">
                   <label for="description" class="form-label">產品描述</label>
                   <textarea id="description" type="text" class="form-control" placeholder="請輸入產品描述"
-                  v-model="productData.description">
+                    v-model="productData.description">
                   </textarea>
                 </div>
                 <div class="mb-3">
                   <label for="content" class="form-label">說明內容</label>
                   <textarea id="content" type="text" class="form-control" placeholder="請輸入說明內容"
-                  v-model="productData.content">
+                    v-model="productData.content">
                   </textarea>
                 </div>
                 <div class="mb-3">
                   <div class="form-check">
                     <input id="is_enabled" class="form-check-input"
                     type="checkbox" :true-value="1" :false-value="0"
-                    v-model="productData.is_enabled">
+                      v-model="productData.is_enabled">
                     <label class="form-check-label" for="is_enabled">是否啟用</label>
                   </div>
                 </div>
               </div>
             </div>
-          </VForm>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+              取消
+            </button>
+            <button type="button" class="btn btn-primary" @click="onSubmit">
+              確認
+            </button>
+          </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-            取消
-          </button>
-          <button type="button" class="btn btn-primary" @click="onSubmit">
-            確認
-          </button>
-        </div>
-      </div>
+      </VForm>
     </div>
   </div>
 </template>
