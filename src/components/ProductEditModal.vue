59625a69c316bd6c2f9201a15fec9c1850fd1bbc
kaiwei <cielchang0324@gmail.com>
2024-02-25 14:24:29 +0800

    feat: 🎸 將按鈕改成submit

--- src/components/ProductEditModal.vue
+++ src/components/ProductEditModal.vue
@@ -105,7 +105,7 @@
             <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
               取消
             </button>
-            <button type="button" class="btn btn-primary" @click="onSubmit">
+            <button type="submit" class="btn btn-primary" @click="onSubmit">
               確認
             </button>
           </div>
